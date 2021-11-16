8e8bc520c01086f92095c12c6e46bfc1a306ea61
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2021-11-16 21:46:52 +0100

    Task 1.1: Finish class Ship

--- src/de/uniluebeck/itm/schiffeversenken/game/model/Ship.java
+++ src/de/uniluebeck/itm/schiffeversenken/game/model/Ship.java
@@ -2,11 +2,15 @@ package de.uniluebeck.itm.schiffeversenken.game.model;
 
 /**
  * This class represents a ship.
- * @author leondietrich, modified by I. Schumacher
+ * @author leondietrich, modified by I. Schumacher & T. Goritz
  *
  */
 public class Ship {
-	
+
+	//Create three private variables for storing
+	private int length;
+	private int hits;
+	private boolean orientation;
 	
 	/**
 	 * This constructor constructs a new ship.
@@ -14,22 +18,32 @@ public class Ship {
 	 * @param orientation The orientation of the ship: true for vertical, false for horizontal
 	 */
 	public Ship(int length, boolean orientation) {
-		
+		this.length      = length;
+		this.hits        = 0;
+		this.orientation = orientation;
 	}
 	
 	/**
-	 *
+	 * Add a hit to the ship if it isn't sunken already
 	 */
-	void hit() {
-		
+	public void hit() {
+		if (!isSunken()) hits++;
 	}
 
 	/**
-	 * 
-	 * 
+	 * Helper method to check if ship is sunken
+	 * @return boolean
 	 */
 	public boolean isSunken() {
-		return false;  
+		return false;
+	}
+
+	/**
+	 * Return the orientation (which is the same like the private orientation variable lol)
+	 * @return boolean
+	 */
+	public boolean isUp() {
+		return orientation;
 	}
 
 }
