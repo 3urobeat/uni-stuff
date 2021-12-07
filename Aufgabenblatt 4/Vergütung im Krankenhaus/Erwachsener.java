ddd251e6e30ace9b09ac0fc27792db7dc615e96e
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2021-12-07 17:36:39 +0100

    Added the template instead of my solution whoops

--- Aufgabenblatt 4/Vergütung im Krankenhaus/Erwachsener.java
+++ Aufgabenblatt 4/Vergütung im Krankenhaus/Erwachsener.java
@@ -3,6 +3,22 @@
  * 
  * Ist von Ihnen anzupassen
  */
-class Erwachsener {
+class Erwachsener extends Patient {
     
+    /**
+     * Konstruktor für einen erwachsenen Patienten
+     * @param name Name des Patienten
+     * @param krankheitsklasse Die Krankheitsklasse (1, 2 oder 3) des Patienten
+     */ 
+    public Erwachsener(String name, int krankheitsklasse) {
+        super(name, krankheitsklasse);
+    }
+    
+    
+    /**
+     * Gibt den Namen des Patienten auf die Konsole aus
+     */ 
+    public void zeigePatient() {
+        System.out.println(this.getName() + " (Erwachsener)");
+    }
 }
