984bb656614efe2fb1828737186324b81672f8ba
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-11-02 17:47:59 +0100

    Init maven folder structure

---
+++ src/main/java/triangle/Main.java
@@ -0,0 +1,13 @@
+package triangle;
+
+/*
+ Compile with:
+ mvn compile
+ mvn exec:java -Dexec.mainClass="triangle.Main"
+ */
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
