8a347c655365dc2893261ed02d629254926a8a10
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-11-02 19:00:18 +0100

    Finished Task 3 of U2

--- src/main/java/triangle/Main.java
+++ src/main/java/triangle/Main.java
@@ -6,8 +6,40 @@ package triangle;
  mvn exec:java -Dexec.mainClass="triangle.Main"
  */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+
+        /* ------------------ U2 - Task 3 ------------------ */
+
+        // Get three ints from stdin using a Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Submit 3 separate ints, representing the 3 sides of our triangle:");
+
+        System.out.print("x: ");
+        int x = scanner.nextInt();
+
+        System.out.print("y: ");
+        int y = scanner.nextInt();
+
+        System.out.print("z: ");
+        int z = scanner.nextInt();
+
+        // Check if triangle is equilateral, isosceles or scalene
+        String state = "";
+
+        if (x == y && y == z) { // equilateral, all sides are equally long
+            state = "gleichseitig & gleichschenklig";
+        } else if (x == y || y == z || x == z) { // isosceles, 2 sides are equally long
+            state = "gleichschenklig";
+        } else { // scalene, nothing matches each other
+            state = "ungleichseitig";
+        }
+
+        // Output result
+        System.out.println("Das angegebene Dreieck ist " + state + ".");
+
     }
 }
