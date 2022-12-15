82c09b3ed74c08bd04860c13ece1e9e8e0382d2e
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-12-16 00:09:03 +0100

    JavaFx GUI stuff that doesn't compile but idc

--- src/main/java/triangle/Main.java
+++ src/main/java/triangle/Main.java
@@ -1,45 +1,60 @@
 package triangle;
 
-/*
- Compile with:
- mvn compile
- mvn exec:java -Dexec.mainClass="triangle.Main"
- */
+import javafx.*;
 
-import java.util.Scanner;
+public class Main extends Application {
+    // Command line entry
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-public class Main {
-    public static void main(String args[]) {
+    @Override
+    public void start(Stage primaryStage) {
 
-        /* ------------------ U2 - Task 3 ------------------ */
+        // Set title
+        primaryStage.setTitle("Ausgabe-fuer-Dreieck-erzeugen-Geraet");
 
-        // Get three ints from stdin using a Scanner
-        Scanner scanner = new Scanner(System.in);
+        // Add 3 input fields
+        TextField xIn = new TextField("x value");
+        TextField yIn = new TextField("y value");
+        TextField zIn = new TextField("z value");
 
-        System.out.println("Submit 3 separate ints, representing the 3 sides of our triangle:");
+        // Create StackPane
+        StackPane root = new StackPane();
+        root.getChildren().add(xIn);
+        root.getChildren().add(yIn);
+        root.getChildren().add(zIn);
 
-        System.out.print("x: ");
-        int x = scanner.nextInt();
+        // create a label
+        Label label = new Label("");
+        root.getChildren().add(label);
 
-        System.out.print("y: ");
-        int y = scanner.nextInt();
+        // Add event to detect input and update label
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                // Check if triangle is equilateral, isosceles or scalene
+                String state = "";
 
-        System.out.print("z: ");
-        int z = scanner.nextInt();
+                int x = Integer.parseInt(xIn.getText());
+                int y = Integer.parseInt(yIn.getText());
+                int z = Integer.parseInt(zIn.getText());
 
-        // Check if triangle is equilateral, isosceles or scalene
-        String state = "";
+                if (x == y && y == z) { // equilateral, all sides are equally long
+                    state = "gleichseitig & gleichschenklig";
+                } else if (x == y || y == z || x == z) { // isosceles, 2 sides are equally long
+                    state = "gleichschenklig";
+                } else { // scalene, nothing matches each other
+                    state = "ungleichseitig";
+                }
 
-        if (x == y && y == z) { // equilateral, all sides are equally long
-            state = "gleichseitig & gleichschenklig";
-        } else if (x == y || y == z || x == z) { // isosceles, 2 sides are equally long
-            state = "gleichschenklig";
-        } else { // scalene, nothing matches each other
-            state = "ungleichseitig";
-        }
+                // Output result
+                label.setText("Das angegebene Dreieck ist " + state + ".");
+            }
+        };
 
-        // Output result
-        System.out.println("Das angegebene Dreieck ist " + state + ".");
+        // Show window with given dimensions
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
 
     }
 }
