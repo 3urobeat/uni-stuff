513b77710d09e94eb0c0c517e6fd0023b570a311
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2023-01-12 21:16:19 +0100

    Fix broken event listener

--- java-uni-triangle-helper/src/main/java/triangle/Main.java
+++ java-uni-triangle-helper/src/main/java/triangle/Main.java
@@ -1,6 +1,7 @@
 package triangle;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -29,22 +30,24 @@ public class Main extends Application {
         // Create StackPane
         GridPane root = new GridPane();
 
+        // Space stuff reasonably
         root.setVgap(20);
         root.setHgap(10);
         root.setAlignment(Pos.CENTER);
 
+        // Place stuff reasonably
         root.add(xIn, 0, 0);
         root.add(yIn, 0, 1);
         root.add(zIn, 0, 2);
 
-        // create a label
+        // Create an output label
         Label label = new Label("test");
         label.setAlignment(Pos.CENTER);
         root.add(label, 0, 3);
 
         // Add event to detect input and update label
-        EventHandler<ActionEvent> event = new EventHandler<>() {
-            public void handle(ActionEvent e) {
+        ChangeListener<String> listener = (observableValue, oldValue, newValue) -> {
+            try {
                 // Check if triangle is equilateral, isosceles or scalene
                 String state = "";
 
@@ -62,8 +65,15 @@ public class Main extends Application {
 
                 // Output result
                 label.setText("Das angegebene Dreieck ist " + state + ".");
+
+            } catch (NumberFormatException e) {
+                label.setText("Eingabe ungültig!");
             }
         };
+
+        xIn.textProperty().addListener(listener);
+        yIn.textProperty().addListener(listener);
+        zIn.textProperty().addListener(listener);
 
         // Show window with given dimensions
         primaryStage.setScene(new Scene(root, 300, 250));
