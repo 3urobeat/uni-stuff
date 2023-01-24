4cf366956b68c081b9c4a82065596d56bac98df0
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2023-01-24 14:41:16 +0100

    Task 3: Create diagram with matplotlib

--- python-data-processing/implementation.py
+++ python-data-processing/implementation.py
@@ -57,6 +57,26 @@ def plot_distance(H):
 			Die Grafik soll dann z.B. durch Aufruf der Funktion `show()` auf den hier 
 			zurueckgegebenen Wert angezeigt werden können.
 	"""
-	# TODO
 
+	# Create a new matplotlib figure using these sizes and give it a name
+	plt.figure(figsize=(10, 7))
+	plt.title("Hellinger-Distanzen")
+
+	# Give both axis labels
+	plt.xlabel("Verteilung (Matrixzeile)")
+	plt.ylabel("Distanz")
+
+	# Create an array of indices for our x axis
+	indexArr = np.arange(0, len(H))
+
+	# Create a bar for each element of H, index on x-axis, value on y-axis
+	plt.bar(indexArr, H, label="HD")
+
+	# Add a legend to the figure which will display our blue bar as "HD" and move it to look like the given example
+	plt.figlegend(loc="upper right", bbox_to_anchor=(0.985, 0.945))
+
+	# Compress everything to use less space
+	plt.tight_layout()
+
+	# Return plt obj so main.py can display it
 	return plt
